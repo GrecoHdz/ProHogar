99fdde2afa404b2c7b1d396b6f19a80d42b052c8
GrecoHdz <grecopavonjh@gmail.com>
2026-03-26 11:23:12 -0600

    correccion correo recuperacion

--- back/src/config/mailer.js
+++ back/src/config/mailer.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true para el puerto 465, false para otros puertos
+    port: 465,
+    secure: true, // true para el puerto 465, false para otros puertos
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
